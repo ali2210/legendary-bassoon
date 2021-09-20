35a765a7b65ad0ccbccff6fc1b355aa0d8b74d79
Ali Hassan <Alideveloper95@gmail.com>
2021-09-20 13:27:37 +0500

    update

--- go.mod
+++ go.mod
@@ -3,10 +3,7 @@ module github.com/ali2210/legendary-bassoon
 go 1.16
 
 require (
-	github.com/ipfs/go-cid v0.0.7 // indirect
 	github.com/ipfs/go-ipfs-http-client v0.1.0 // indirect
-	github.com/ipfs/interface-go-ipfs-core v0.4.0 // indirect
-	github.com/libp2p/go-libp2p-core v0.9.0 // indirect
-	github.com/multiformats/go-multiaddr v0.4.0 // indirect
-	github.com/multiformats/go-multihash v0.0.15 // indirect
+	github.com/ipfs/interface-go-ipfs-core v0.5.2 // indirect
+	github.com/libp2p/go-libp2p-core v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,11 +5,11 @@ package main
 import (
 	http_ipfs_client "github.com/ipfs/go-ipfs-http-client"
 	"fmt"
-	 "context"
-	p2ps "github.com/libp2p/go-libp2p-core/peer"
+	// "context"
+	// p2ps "github.com/libp2p/go-libp2p-core/peer"
 	paths "github.com/ipfs/interface-go-ipfs-core/path"
 	core "github.com/ipfs/interface-go-ipfs-core"
-	multiAddr "github.com/multiformats/go-multiaddr"
+	//multiAddr "github.com/multiformats/go-multiaddr"
 	gocid "github.com/ipfs/go-cid"
 	mulhash "github.com/multiformats/go-multihash"
 	"crypto/sha256"
@@ -33,6 +33,7 @@ func main() {
 	fmt.Println("@http-client:", client_http_api)
 	block := client_http_api.Block()
 	fmt.Println("@block:", block)
+	fmt.Printf("Type: %T\n", block)
 	
 	hash := sha256.Sum256([]byte("hello-world"))
 	hex_Data, err  := hex.DecodeString(hex.EncodeToString(hash[:])); if err != nil {
@@ -56,23 +57,6 @@ func main() {
 	
 	resolved := paths.IpfsPath(cid)
 	fmt.Println("resolved _ ipfs:", resolved)
-	
-	pId, err := p2ps.Decode(resolved.Cid().String()); if err != nil {
-		fmt.Println("peer id error:", err.Error())
-		return 
-	}
-
-	fmt.Println("@peer _ id:", pId)
-	addr , err := multiAddr.NewMultiaddr(multaddr.String()); if err != nil {
-		fmt.Println("multi-address error:", err.Error())
-		return 
-	}	
-	fmt.Println("@multi-address:", addr)
-	addr_info, err := p2ps.AddrInfoFromString("/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWKSG7DZhTNLBqkRzzkvSBbThZ1EpWBsDP54iq6mWLkiv6"); if err != nil {
-		fmt.Println("peer multi-address error:", err.Error())
-		return 
-	}
-	fmt.Println("@peer_multiaddr_info:", addr_info)	
-	var object core.ObjectAPI = core.Object()
-	fmt.Println("@object:", object)
+	var goipfs_core core.ObjectAPI
+	fmt.Println("core object:", goipfs_core)
 }
